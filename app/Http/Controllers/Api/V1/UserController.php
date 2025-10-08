9242d81a36fe28b9ea47b1a5d58e2f62b535069b
dyingpigeon <the.farhanad123@gmail.comm>
2025-10-08 15:04:43 +0700

    fix tugas

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -4,20 +4,27 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\UpdateUserRequest;
+use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 // use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function update(UpdateUserRequest $request, $id)
+    // public function update(UpdateUserRequest $request, $id)
+    // {
+    //     $user = User::findOrFail($id);
+
+    //     $user->update($request->only('name'));
+
+    //     return response()->json([
+    //         'message' => 'User name updated successfully!',
+    //         'data' => $user,
+    //     ]);
+    // }
+
+        public function update(UpdateUserRequest $request, User $user)
     {
-        $user = User::findOrFail($id);
-
-        $user->update($request->only('name'));
-
-        return response()->json([
-            'message' => 'User name updated successfully!',
-            'data' => $user,
-        ]);
+        $user->update($request->all());
+        return new UserResource($user);
     }
 }
